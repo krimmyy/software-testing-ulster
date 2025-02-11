1828801a5783b8db713d5dcc8057acd1116969e8
Eligijus Ciza <eligijusguga@gmail.com>
2025-02-11 11:25:15 +0000

    updated FormSubmissionTest.java

--- src/main/java/Week2/FormSubmissionTest.java
+++ src/main/java/Week2/FormSubmissionTest.java
@@ -10,18 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FormSubmissionTest {
     @Test
     void testLogin() {
-        System.setProperty("webdriver.chrome.driver",
-                "C:\\Users\\B00988791\\Downloads\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\B00988791\\Downloads\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://example.com/login");
+        driver.get("https://practicetestautomation.com/practice-test-login/");
         WebElement username = driver.findElement(By.id("username"));
         WebElement password = driver.findElement(By.id("password"));
 
         WebElement loginButton = driver.findElement(By.id("login"));
-        username.sendKeys("testuser");
-        password.sendKeys("password123");
+        username.sendKeys("student");
+        password.sendKeys("Password123");
         loginButton.click();
-        String expectedUrl = "https://example.com/dashboard";
+        String expectedUrl = "https://practicetestautomation.com/logged-in-successfully/";
         assertEquals(expectedUrl, driver.getCurrentUrl());
         driver.quit();
     }
